382a0d0fddf9dbbf2cc3635b1e9c1da1b3282a23
gbeaufort <guillaume.beaufort@viacesi.fr>
2024-11-06 15:59:19 +0100

    wow

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <unistd.h>
 #include "function.h"
+#include <SDL2/SDL_ttf.h>
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define FPS 60
@@ -255,7 +256,7 @@ void KeyPressed(SDL_Keycode touche){
             vxb = 3;
             break;
         case SDLK_p:
-            printf("%d\n ",points);
+            printf("%d\n",points);
             break;        
         case SDLK_LEFT:
             if(xplat>2){
